1227f2db2bdf07c8c5e9c44ff586bad0732cc461
Jagdish Singh <75190678+Jagdish91@users.noreply.github.com>
2026-01-21 11:27:55 -0700

    Update eip.tf

--- using-remote-state/networking-team/eip.tf
+++ using-remote-state/networking-team/eip.tf
@@ -2,19 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-demo-007-statefile"
-    key    = "networking.tfstate"
-    region = "us-east-1"
-    use_lockfile = true
-  }
-}
-
 resource "aws_eip" "temp" {
   domain = "vpc"
-}
-
-output "eip_address" {
-  value = aws_eip.temp.public_ip
 }
